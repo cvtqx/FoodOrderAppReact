455c7e6b6e8d6ed2491cb920f198e1b380d0705e
Olga Yudkin <cvtqx@protonmail.com>
2023-06-19 17:03:36 -0400

    additem button functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,28 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
+
 
 const HeaderCartButton = props =>{
-        return <button className = {classes.button} onClick={props.onClick}> 
+
+    const cartCtx = useContext(CartContext);
+
+    //use reduce (NOT items.length) to calculate the number of items in the cart because some items might have the same name but different amount
+    
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount
+    }, 0)
+
+        return (
+        <button className = {classes.button} onClick={props.onClick}> 
             <span className = {classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className = {classes.badge}>3</span>
+            <span className = {classes.badge}>{numberOfCartItems}</span>
         </button>
+        )
 }
 
 export default HeaderCartButton;
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,14 @@
+import React from 'react'; 
 import classes from './Input.module.css';
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
     return (
     <div className={classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input}/>  
+        <input ref={ref} {...props.input}/>  
         {/* ...props.input ensures that all key-value pairs of input element are added as props*/}
     </div>
     )
-}
+});
 
 export default Input;
